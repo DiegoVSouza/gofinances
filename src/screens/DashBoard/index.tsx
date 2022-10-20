a1e199f212730ecba1b2d47ba29bb07d6bc29791
Diego Souza <dvsdiegovieira@gmail.com>
2022-10-20 07:57:55 -0300

    Feat: transections

--- src/screens/DashBoard/index.tsx
+++ src/screens/DashBoard/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import { HighLightCard } from "../../components/HighLightCard";
-import { Container, Header, Photo, User, UserGreeeting, UserInfo, UserName, UserWrapper, Icon, HighLightCards } from "./styles";
+import { TransectionCard, TransectionCardProps } from "../../components/TransectionCard";
+import { Container, Header, Photo, User, UserGreeeting, UserInfo, UserName, UserWrapper, Icon, HighLightCards, Transections, Title, TransectionList } from "./styles";
 
+export interface DataListProps extends TransectionCardProps {
+    id: string;
+}
 export function Dashboard() {
+    const data: DataListProps[] = [{
+        id: '123',
+        type: 'positive',
+        title: 'Desenvolvimento de site',
+        amount: "12.000",
+        category:
+        {
+            name: 'Vendas',
+            icon: 'dollar-sign'
+        }
+        ,
+        date: '20/05/2022'
+    }]
     return (
         <Container>
             <Header>
@@ -21,6 +38,14 @@ export function Dashboard() {
                 <HighLightCard />
 
             </HighLightCards>
+
+            <Transections>
+                <Title>Listagem</Title>
+                <TransectionList
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransectionCard data={item} />} />
+            </Transections>
         </Container>
     )
 }
--- src/screens/DashBoard/styles.ts
+++ src/screens/DashBoard/styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components/native";
+import { FlatList } from "react-native";
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 import { Feather } from '@expo/vector-icons'
+import { DataListProps } from ".";
 
 export const Container = styled.View`
     flex: 1;
@@ -52,6 +54,28 @@ export const HighLightCards = styled.ScrollView.attrs({
     horizontal: true,
     showsHorizontalScrollIndicator: false,
     contentContainerStyle: { paddingHorizontal: 24 }
+})`
+    
+`
+
+export const Transections = styled.View`
+    flex: 1;
+    padding: 0 24px;
+
+    margin-top: ${RFPercentage(12)}px;
+`
+
+export const Title = styled.Text`
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+
+`
+
+export const TransectionList = styled(FlatList as new () => FlatList<DataListProps>).attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: {
+        paddingBottom: getBottomSpace()
+    }
 })`
     
 `
